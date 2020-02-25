c4aea4e6b93670966c5bb7ea733243c6a62bd206
ClankyELK <59914233+ClankyELK@users.noreply.github.com>
2020-02-25 14:47:23 +0100

    Updated oblig_2_prototype file
    added some new text to oblig_2_prototype almost done with it just a bit more

--- Prototype.js
+++ Prototype.js
@@ -1,4 +1,6 @@
-let currentlySelectedBar;
+let currentlySelectedBar = "None selected";
+let pooHead = '💩Oy!, WROONNG vErLuE!💩 PoO oN hEaD';
+
 function selectedBar(barSelected) {
 if (currentlySelectedBar == barSelected) {
     currentlySelectedBar = "None selected";
@@ -7,6 +9,25 @@ if (currentlySelectedBar == barSelected) {
 show();
 console.log(barSelected);
 }
-function removeBar(removeSelectedBar) {
- if (currentlySelectedBar == ) 
+function removeSelectedBar() {
+numbers.splice(parseInt(currentlySelectedBar) - 1, 1)
+show();
+}
+function editSelectedBar() {
+let display = parseInt(document.getElementById('displayValue').value);
+console.log(display)
+if (display >= 1 && display <= 10){
+numbers[currentlySelectedBar - 1] = display
+} else {alert(pooHead)}
+
+show();
+} 
+
+function addSelectedBar() {
+let display = parseInt(document.getElementById('displayValue').value);
+if (display >= 1 && display <= 10){
+numbers.push(display)
+} else {alert(pooHead)}
+
+show();
 }
