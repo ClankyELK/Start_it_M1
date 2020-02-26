4211d6824b2000a38928070ddd04b942bc4431e1
ClankyELK <59914233+ClankyELK@users.noreply.github.com>
2020-02-26 14:20:52 +0100

    I finished up oblig_2 and put it in a new file called oblig_2

---
+++ PrototypeOfobjectsInArray/Model.js
@@ -0,0 +1,18 @@
+function createView() {
+
+document.getElementById('contentDiv').innerHTML = 
+`<table>
+<tr>
+<th>M</th> <th>V</th> <th>C</th>
+</tr> <tr>
+<td></td> <td></td> <td></td>
+</tr>
+</table>`;
+console.log('pooHead')
+}
+function createModel() {
+
+}
+function createController() {
+
+}
